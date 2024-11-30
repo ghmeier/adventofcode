eb4a533feb800bcbfb94a326511b2d11401462be
Garret Meier <garret@withpavilion.com>
2024-11-30 11:19:29 -0800

    feat: add 2021 12,13,14 and 2024 1

---
+++ src/2021/day12/index.ts
@@ -0,0 +1,78 @@
+import { intersection } from "lodash";
+import { handleLines } from "../../utils";
+
+const DATA_PATH = `${import.meta.dir}/data.txt`;
+const CALIBRATE_PATH = `${import.meta.dir}/calibrate.txt`;
+const LARGEST_CALIBRATE_PATH = `${import.meta.dir}/calibrate-largest.txt`;
+
+type Cave = { paths: Set<string>; unlimited: boolean };
+type Caves = Record<string, Cave>;
+
+function isUpper(s: string) {
+	return !!s.match(/[A-Z]/);
+}
+
+async function parse(file: string) {
+	const graph: Caves = {};
+
+	await handleLines(file, (l) => {
+		const [s, e] = l.split("-");
+		if (!graph[s]) {
+			graph[s] = { paths: new Set<string>(), unlimited: isUpper(s) };
+		}
+		if (!graph[e]) {
+			graph[e] = { paths: new Set<string>(), unlimited: isUpper(e) };
+		}
+		graph[s].paths.add(e);
+		graph[e].paths.add(s);
+	});
+	return graph;
+}
+
+function visit(graph: Caves, c: string, path: string[]): string[][] {
+	const next = [...path, c];
+	if (c === "end") return [next];
+	if (!graph[c].unlimited && path.includes(c)) return [];
+
+	const paths: string[][] = [];
+	for (const [n] of graph[c].paths.entries()) {
+		paths.push(...visit(graph, n, next));
+	}
+
+	return paths;
+}
+
+function visitDouble(
+	graph: Caves,
+	c: string,
+	path: string[],
+	double: string | null,
+): string[][] {
+	const next = [...path, c];
+	if (c === "end") return [next];
+	if (c === "start" && path.length) return [];
+    const hasVisited = !graph[c].unlimited && path.filter((p) => p === c).length >= 1;
+	if (hasVisited && double) return [];
+	const nextDouble = double || (hasVisited && c) || null;
+	const paths: string[][] = [];
+	for (const [n] of graph[c].paths.entries()) {
+		paths.push(...visitDouble(graph, n, next, nextDouble));
+	}
+	return paths;
+}
+
+async function problemOne() {
+	const graph = await parse(DATA_PATH);
+	const paths = visit(graph, "start", []);
+
+	console.log("Problem one:", paths.length);
+}
+
+async function problemTwo() {
+	const graph = await parse(DATA_PATH);
+	const paths = visitDouble(graph, "start", [], null);
+	console.log("Problem two:", paths.length);
+}
+
+await problemOne();
+await problemTwo();
---
+++ src/2021/day13/index.ts
@@ -0,0 +1,94 @@
+import { range } from "lodash";
+import { type Point, dumpGrid, handleLines, iterateGrid } from "../../utils";
+
+const DATA_PATH = `${import.meta.dir}/data.txt`;
+const CALIBRATE_PATH = `${import.meta.dir}/calibrate.txt`;
+
+type Orientation = "x" | "y";
+
+async function parse(file: string) {
+	const grid: string[][] = [];
+	let maxX = 0;
+	let maxY = 0;
+	const points: Point[] = [];
+	const folds: [Orientation, number][] = [];
+	await handleLines(file, (l) => {
+		if (l.startsWith("fold along ")) {
+			const [axis, line] = l.replace("fold along ", "").split("=");
+			folds.push([axis as Orientation, Number.parseInt(line, 10)]);
+			return;
+		}
+		const point = l.split(",").map((p) => Number.parseInt(p, 10)) as Point;
+		if (point[0] > maxX) maxX = point[0];
+		if (point[1] > maxY) maxY = point[1];
+		points.push(point);
+	});
+
+	for (let y = 0; y <= maxY; y++) {
+		grid.push(range(0, maxX + 1).map(() => "."));
+	}
+	for (const [x, y] of points) {
+		grid[y][x] = "#";
+	}
+
+	return { grid, folds };
+}
+
+function foldX(grid: string[][], line: number) {
+	for (let y = 0; y < grid.length; y++) {
+		for (let x = line + 1; x < grid[0].length; x++) {
+			if (grid[y][x] === ".") continue;
+			grid[y][line - (x - line)] = "#";
+			grid[y][x] = ".";
+		}
+	}
+	return grid.map((r) => r.slice(0, line));
+}
+
+function foldY(grid: string[][], line: number) {
+	for (let x = 0; x < grid[0].length; x++) {
+		for (let y = line + 1; y < grid.length; y++) {
+			if (grid[y][x] === ".") continue;
+			grid[line - (y - line)][x] = "#";
+			grid[y][x] = ".";
+		}
+	}
+	return grid.slice(0, line);
+}
+
+async function problemOne() {
+	let { grid, folds } = await parse(DATA_PATH);
+	dumpGrid(grid);
+
+	for (const [axis, line] of folds) {
+		console.log(`Fold ${axis}=${line}`);
+		if (axis === "y") grid = foldY(grid, line);
+		if (axis === "x") grid = foldX(grid, line);
+		dumpGrid(grid);
+		break;
+	}
+
+	let count = 0;
+	iterateGrid(grid, (x, y) => {
+		if (grid[y][x] === "#") count += 1;
+	});
+
+	console.log("Problem one:", count);
+}
+
+async function problemTwo() {
+    let { grid, folds } = await parse(DATA_PATH);
+    dumpGrid(grid);
+
+    for (const [axis, line] of folds) {
+        console.log(`Fold ${axis}=${line}`);
+        if (axis === "y") grid = foldY(grid, line);
+        if (axis === "x") grid = foldX(grid, line);
+        dumpGrid(grid);
+    }
+
+	console.log("Problem two:", null);
+}
+
+await problemOne();
+await problemTwo();
